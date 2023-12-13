0a1d8738942f443be144486a87b17c1a9f1ce2f4
Djaparov Ramil <rdb03@mail.ru>
2023-12-13 16:56:42 +0600

    Добавил react-router-dom чтобы при переходе на несуществующую страницу пользователь попдал на not found. Делатькрутилки смысла не вижу, никаких запросов нет

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
 import DoorLock from "./containers/DoorLock/DoorLock.tsx";
+import NoFound from "./components/NoFound/NoFound.tsx";
 
 const App = () => {
   return (
     <>
-        <DoorLock/>
+        <Routes>
+            <Route path="/" element={<DoorLock />} />
+            <Route path="*" element={<NoFound />} />
+            </Routes>
     </>
   )
 };
---
+++ src/components/NoFound/NoFound.tsx
@@ -0,0 +1,9 @@
+const NoFound = () => {
+    return (
+        <div>
+            <h1>No Found</h1>
+        </div>
+    );
+};
+
+export default NoFound;
